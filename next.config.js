48c62ad8ffb921ec654f1f94e212456999ffda98
Iuliia <yulyashastr@gmail.com>
2026-03-17 09:52:56 +0100

    next config changes for gh

--- next.config.js
+++ next.config.js
@@ -4,17 +4,20 @@ const path = require('path')
 
 const assetsHost = process.env.ASSETS_HOST
 const basePath = process.env.GITHUB_PAGES_BASE_PATH || ''
+const isGhPages = Boolean(basePath)
 
 module.exports = {
-  output: basePath ? 'export' : undefined,
+  output: isGhPages ? 'export' : undefined,
   basePath: basePath || undefined,
   assetPrefix: assetsHost || (basePath ? basePath + '/' : undefined),
   trailingSlash: true,
-  i18n: {
-    locales: ['ru', 'en'],
-    defaultLocale: 'ru',
-    localeDetection: false
-  },
+  i18n: isGhPages
+    ? undefined
+    : {
+        locales: ['ru', 'en'],
+        defaultLocale: 'ru',
+        localeDetection: false
+      },
   cleanDistDir: false,
   swcMinify: true,
   sassOptions: {
@@ -29,6 +32,7 @@ module.exports = {
     scrollRestoration: true
   },
   images: {
+    unoptimized: isGhPages,
     // deviceSizes: [600, 960, 1280, 1920, 2650],
     // path: assetsHost ? `${assetsHost}/_next/image` : undefined,
     domains: ['station.garagemca.radio', 'station.garagemca.radio', 'cdn-ec-static.garagemca.org']
